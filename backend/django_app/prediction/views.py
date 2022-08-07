aaca40e9b3d2e969de8a6f88c364293bcab1c33b
Mingyuan Ren <m28ren@uwaterloo.ca>
2022-08-07 04:46:08 -0400

    emojify

---
+++ backend/django_app/prediction/views.py
@@ -0,0 +1,206 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from prediction.apps import PredictionConfig
+import pandas as pd
+import csv
+import numpy as np
+import emoji
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+from emo_utils import *
+import emoji
+from test_utils import *
+word_to_index, index_to_word, word_to_vec_map = read_glove_vecs('data/glove.6B.50d.txt')
+X_train, Y_train = read_csv('data/train_emoji.csv')
+X_test, Y_test = read_csv('data/tesss.csv')
+maxLen = len(max(X_train, key=len).split())
+# Create your views here.
+# Class based view to predict based on Emojify model
+# class IRIS_Model_Predict(APIView):
+class Emojify_Model_Predict(APIView):
+    def post(self, request, format=None):
+        data = request.data
+        keys = []
+        values = []
+        for key in data:
+            keys.append(key)
+            values.append(data[key])
+        # X = pd.Series(values).to_numpy().reshape(1, -1)
+        X = np.array([values])
+        X_test_indices = sentences_to_indices(X, word_to_index, maxLen)
+        # loaded_mlmodel = PredictionConfig.mlmodel 
+        emojify = keras.models.load_model("mlmodel/emojify")
+        y_pred = emojify.predict(X_test_indices)
+        print(values +' '+  label_to_emoji(np.argmax(y_pred)))
+        y_pred = pd.Series(y_pred)
+        target_map = {"0": "\u2764\uFE0F",    # :heart: prints a black instead of red heart depending on the font
+                    "1": ":baseball:",
+                    "2": ":smile:",
+                    "3": ":disappointed:",
+                    "4": ":fork_and_knife:"}
+        y_pred = y_pred.map(target_map).to_numpy()
+        
+        response_dict = {"Predicted Emoji": y_pred[0]}
+        return Response(response_dict, status=200)
+    
+
+def read_glove_vecs(glove_file):
+    with open(glove_file, 'r') as f:
+        words = set()
+        word_to_vec_map = {}
+        for line in f:
+            line = line.strip().split()
+            curr_word = line[0]
+            words.add(curr_word)
+            word_to_vec_map[curr_word] = np.array(line[1:], dtype=np.float64)
+        
+        i = 1
+        words_to_index = {}
+        index_to_words = {}
+        for w in sorted(words):
+            words_to_index[w] = i
+            index_to_words[i] = w
+            i = i + 1
+    return words_to_index, index_to_words, word_to_vec_map
+
+# GRADED FUNCTION: sentences_to_indices
+
+def sentences_to_indices(X, word_to_index, max_len):
+    """
+    Converts an array of sentences (strings) into an array of indices corresponding to words in the sentences.
+    The output shape should be such that it can be given to `Embedding()` (described in Figure 4). 
+    
+    Arguments:
+    X -- array of sentences (strings), of shape (m, 1)
+    word_to_index -- a dictionary containing the each word mapped to its index
+    max_len -- maximum number of words in a sentence. You can assume every sentence in X is no longer than this. 
+    
+    Returns:
+    X_indices -- array of indices corresponding to words in the sentences from X, of shape (m, max_len)
+    """
+    
+    m = X.shape[0]                                   # number of training examples
+    
+    ### START CODE HERE ###
+    # Initialize X_indices as a numpy matrix of zeros and the correct shape (≈ 1 line)
+    X_indices = np.zeros((m, max_len))
+    
+    for i in range(m):                               # loop over training examples
+        
+        # Convert the ith training sentence in lower case and split is into words. You should get a list of words.
+        sentence_words = X[i].lower().split()
+        
+        # Initialize j to 0
+        j = 0
+        
+        # Loop over the words of sentence_words
+
+        for w in sentence_words:
+            # if w exists in the word_to_index dictionary
+            if w in word_to_index:
+                # Set the (i,j)th entry of X_indices to the index of the correct word.
+                X_indices[i, j] = word_to_index[w]
+                # Increment j to j + 1
+                j =  j+1
+            
+    ### END CODE HERE ###
+    
+    return X_indices
+
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
+
+
+def read_csv(filename = 'data/emojify_data.csv'):
+    phrase = []
+    emoji = []
+
+    with open (filename) as csvDataFile:
+        csvReader = csv.reader(csvDataFile)
+
+        for row in csvReader:
+            phrase.append(row[0])
+            emoji.append(row[1])
+
+    X = np.asarray(phrase)
+    Y = np.asarray(emoji, dtype=int)
+
+    return X, Y
+
+def convert_to_one_hot(Y, C):
+    Y = np.eye(C)[Y.reshape(-1)]
+    return Y
+
+
+emoji_dictionary = {"0": "\u2764\uFE0F",    # :heart: prints a black instead of red heart depending on the font
+                    "1": ":baseball:",
+                    "2": ":smile:",
+                    "3": ":disappointed:",
+                    "4": ":fork_and_knife:"}
+
+def label_to_emoji(label):
+    """
+    Converts a label (int or string) into the corresponding emoji code (string) ready to be printed
+    """
+    return emoji.emojize(emoji_dictionary[str(label)], use_aliases=True)
+              
+    
+def print_predictions(X, pred):
+    print()
+    for i in range(X.shape[0]):
+        print(X[i], label_to_emoji(int(pred[i])))
+        
+
+def predict(X, Y, W, b, word_to_vec_map):
+    """
+    Given X (sentences) and Y (emoji indices), predict emojis and compute the accuracy of your model over the given set.
+    
+    Arguments:
+    X -- input data containing sentences, numpy array of shape (m, None)
+    Y -- labels, containing index of the label emoji, numpy array of shape (m, 1)
+    
+    Returns:
+    pred -- numpy array of shape (m, 1) with your predictions
+    """
+    m = X.shape[0]
+    pred = np.zeros((m, 1))
+    any_word = list(word_to_vec_map.keys())[0]
+    # number of classes  
+    n_h = word_to_vec_map[any_word].shape[0] 
+    
+    for j in range(m):                       # Loop over training examples
+        
+        # Split jth test example (sentence) into list of lower case words
+        words = X[j].lower().split()
+        
+        # Average words' vectors
+        avg = np.zeros((n_h,))
+        count = 0
+        for w in words:
+            if w in word_to_vec_map:
+                avg += word_to_vec_map[w]
+                count += 1
+        
+        if count > 0:
+            avg = avg / count
+
+        # Forward propagation
+        Z = np.dot(W, avg) + b
+        A = softmax(Z)
+        pred[j] = np.argmax(A)
+        
+    print("Accuracy: "  + str(np.mean((pred[:] == Y.reshape(Y.shape[0],1)[:]))))
+    
+    return pred
